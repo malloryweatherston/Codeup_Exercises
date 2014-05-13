dd07081441c6289dbbb106c178bee00f4c1d31bd
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-13 12:07:12 -0500

    completed editing on fizzbuzz, added == operator

---
+++ fizzbuzz.php
@@ -0,0 +1,20 @@
+<?php
+
+//for($i = 1; $i <= 100; $i = $i + 1) {
+   //if ($i % 5 = 0) {
+  //  echo "FIZZ\n";
+//} elseif ($i % 3 = 0) {
+//	echo "BUZZ\n";
+//} if ($i % 5 = 0 && $i % 3 = 0) {
+//	echo "FIZZBUZZ\n";
+//} }
+
+for($i = 1; $i <= 100; $i = $i + 1) {
+  if ($i % 5 == 0 && $i % 3 == 0) {
+	echo "FIZZBUZZ\n";
+} elseif ($i % 3 == 0) {
+	echo "FIZZ\n";
+} elseif ($i % 5 == 0) {
+    echo "BUZZ\n";
+} else echo "{$i}\n";
+}
