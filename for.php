3b6d08d52a7c40aa2aafbf53450c1b3170e66536
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-13 11:21:37 -0500

    added comment lines

--- for.php
+++ for.php
@@ -1,23 +1,23 @@
 <?php 
-
+//Prompt the user for a minimum number
 fwrite(STDOUT, "Please enter the minimum number\n");
-
+//assign input to a variable
 $minimum = trim(fgets(STDIN)); 
-
+//Prompt the user for a maximum number
 fwrite(STDOUT, "Please enter the maximum number\n");
-
+//assign input to a variable
 $maximum = trim(fgets(STDIN)); 
-
+//Tell the user if their input is a non-numeric value
 if (!is_numeric($minimum) || !is_numeric($maximum)) {
 	echo "You must enter a numeric value!\n";
 	exit(1);
 }
-
+//output a for loop
 for($i = $minimum; $i <= $maximum; $i = $i+1) {
 	echo "{$i}\n";
 }
 fwrite(STDOUT, "Please choose an increment\n");
-
+//assign input to a variable and if statement to see set default 
 $increment = trim(fgets(STDIN)); 
 if ($increment == "") {
 	$increment = 1;
