405795533e75267b0a5111ba7d450b969b1375b9
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-13 13:17:09 -0500

    fixed bracket issue/ formatting

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,12 +1,13 @@
 <?php
 
 for($i = 1; $i <= 100; $i = $i + 1) {
-  if ($i % 5 == 0 && $i % 3 == 0) {
-	echo "FIZZBUZZ\n";
-} elseif ($i % 3 == 0) {
-	echo "FIZZ\n";
-} elseif ($i % 5 == 0) {
-    echo "BUZZ\n";
-} else {
-	echo "{$i}\n";
-} }
+ 	 if ($i % 5 == 0 && $i % 3 == 0) {
+		echo "FIZZBUZZ\n";
+	} elseif ($i % 3 == 0) {
+		echo "FIZZ\n";
+	} elseif ($i % 5 == 0) {
+    	echo "BUZZ\n";
+	} else {
+		echo "{$i}\n";
+	} 
+}
