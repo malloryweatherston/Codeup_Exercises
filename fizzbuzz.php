9006e59c709ea82a472b9a0f780421ccc1939bf2
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-13 12:08:35 -0500

    deleted superfluous info

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,14 +1,5 @@
 <?php
 
-//for($i = 1; $i <= 100; $i = $i + 1) {
-   //if ($i % 5 = 0) {
-  //  echo "FIZZ\n";
-//} elseif ($i % 3 = 0) {
-//	echo "BUZZ\n";
-//} if ($i % 5 = 0 && $i % 3 = 0) {
-//	echo "FIZZBUZZ\n";
-//} }
-
 for($i = 1; $i <= 100; $i = $i + 1) {
   if ($i % 5 == 0 && $i % 3 == 0) {
 	echo "FIZZBUZZ\n";
