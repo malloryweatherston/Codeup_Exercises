1e1359c252ba55fbf46e412756b9d339e0f0d3ff
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-13 11:16:27 -0500

    completed editing, added is_numeric

--- for.php
+++ for.php
@@ -1,28 +1,28 @@
 <?php 
 
-//if ($argc < 3) {
-  //   $progName = $argv[0];
-    // echo "Please enter a starting number and an ending number!\n";
-     //exit(1);
-//}
-
-//$minimum = $argv[1];
-//$maximum = $argv[2];
-
 fwrite(STDOUT, "Please enter the minimum number\n");
 
-$minimum = fgets(STDIN); 
+$minimum = trim(fgets(STDIN)); 
 
 fwrite(STDOUT, "Please enter the maximum number\n");
 
-$maximum = fgets(STDIN); 
+$maximum = trim(fgets(STDIN)); 
+
+if (!is_numeric($minimum) || !is_numeric($maximum)) {
+	echo "You must enter a numeric value!\n";
+	exit(1);
+}
 
 for($i = $minimum; $i <= $maximum; $i = $i+1) {
 	echo "{$i}\n";
 }
 fwrite(STDOUT, "Please choose an increment\n");
 
-$increment = fgets(STDIN); 
+$increment = trim(fgets(STDIN)); 
+if ($increment == "") {
+	$increment = 1;
+}
 for($i = $minimum; $i <= $maximum; $i = $i + $increment) {
 	echo "{$i}\n";
 }
+
