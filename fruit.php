c5ba4cd0addfd28f8d6a2f00180faad9aac17848
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-14 09:04:20 -0500

    added starting steps

--- fruit.php
+++ fruit.php
@@ -1,5 +1,14 @@
 <?php
 
+//$fruits = array(apple, banana, strawberry, blueberry, peach); 
+
+//foreach($fruits as $fruit) {
+//	echo "{$fruit}\n";
+//}
+
+
+
+
 $fruits = array(
 	array('name' => apple, 'color' => green),
 	array('name' => banana, 'color' => yellow),
@@ -13,6 +22,7 @@ $fruits = array(
 foreach($fruits as $fruit) {
 	foreach ($fruit as $key => $value) {
 		echo "The $key is $value\n";
+
 	}
 }
 
