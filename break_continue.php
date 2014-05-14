3e7de275850322ff31ffa979892bbb43e29dbf91
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-14 11:04:35 -0500

    Step 2 : Create another for loop that counts from 1 to 100, but stops after 10 using break

--- break_continue.php
+++ break_continue.php
@@ -1,8 +1,15 @@
 <?php
 
+ for ($i = 1; $i <= 100; $i = $i + 1) {
+     if ($i % 2 == 0) {
+     	echo $i . "\n";
+         continue; 
+     } 
+ } 
+
 for ($i = 1; $i <= 100; $i = $i + 1) {
-    if ($i % 2 == 0) {
-    	echo $i . "\n";
-        continue; 
+	echo $i . "\n";
+    if ($i == 10) {
+        break; 
     } 
 } 
