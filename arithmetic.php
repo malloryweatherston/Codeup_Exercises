d0232368bc0ef9bec5a1ee8e87725a9066aa61ee
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-15 09:47:25 -0500

    created functions for add, subtract, multiply, divide, modulus, and tested functions

---
+++ arithmetic.php
@@ -0,0 +1,31 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . "\n";
+}
+
+add(5,4);
+
+function subtract($a, $b) {
+    echo $a - $b . "\n";
+}
+
+subtract(7,4);
+
+function multiply($a, $b) {
+    echo $a * $b . "\n";
+}
+multiply(5,4);
+
+function divide($a, $b) {
+    echo $a / $b . "\n";
+}
+
+divide(10,2);
+
+function modulus($a, $b) {
+	echo $a % $b . "\n";
+}
+modulus(2,2);
+
+?>
