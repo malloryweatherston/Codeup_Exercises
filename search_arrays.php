b436128048749112dab8e6ff6a4b288cd3210933
malloryweatherston <mjw0909@sbcglobal.net>
2014-05-19 14:26:27 -0500

    added a function to see if the value was true or false

--- search_arrays.php
+++ search_arrays.php
@@ -5,8 +5,29 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-$query = 'Tina';
+//$query = 'Tina' || 'Bob';
 
-$result = array_search($query, $names); 
+//$result = array_search($query, $names); 
 
-var_dump($result);
+//var_dump($result);
+
+function array_has_value($needle, $haystack) {
+ $result = array_search($needle, $haystack); 
+ if ($result === false ) {
+	return false;
+	} else {
+		return true; 
+	}
+
+return array_search($needle, $haystack); 
+}
+
+//$result2 = array_search($query2, $compare);
+
+//var_dump($result2); 
+
+$query = 'Tina' || 'Bob';
+
+$result = array_search($query, $names);
+
+var_dump(array_has_value($result));
