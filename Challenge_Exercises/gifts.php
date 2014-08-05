cc22902417359626f0f34d2366660d8a7352876d
malloryweatherston <mjw0909@sbcglobal.net>
2014-08-05 11:44:13 -0500

    Completed simple array exercise review.

---
+++ Challenge_Exercises/gifts.php
@@ -0,0 +1,12 @@
+<?php 
+
+$gift = array('purse', 'ring', 'bracelet', 'scarf', 'candle', 'soap', 'necklace'); 
+
+
+// $rand_keys = array_rand($gifts);
+
+
+
+foreach ($gift as $gifts) {
+    echo "$gifts ";
+}
